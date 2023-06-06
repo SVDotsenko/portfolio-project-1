7c4910719577b421f792d852543d9e9818eadc8b
Serhii Dotsenko <serdotsenko@gmail.com>
2023-06-06 08:22:23 +0300

    дз 4. c бургер-меню, но ещё с неправильной иконкой

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-$(function () {
+$(() => {
+    $('.menu__btn').on('click', () => {
+        $('.header').css('background-image', (_, value) => value === 'none' ? 'url(../images/header-bg.jpg)' : 'none');
+        $('.header__content-inner').toggle();
+        $('.menu__list').toggleClass('menu__list--active');
+    });
     const mixer = mixitup('.portfolio__content');
 });
