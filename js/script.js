4610b0cb893ffe23ed4dd27eb8ec70f9a9ef0d3e
Serhii Dotsenko <serdotsenko@gmail.com>
2023-06-13 13:46:47 +0300

    рефакторинг после code-review дз 5. липкий header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 $(() => {
-    $('.burger__button').on('click', () => {
+    $(".burger__button, .menu a").on("click", () => {
         $('.menu__list, .burger__button').toggleClass('active');
         $('body').toggleClass('lock');
+    });
+
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    const isIE = /Trident|MSIE/.test(window.navigator.userAgent);
+
+    if (isSafari || isIE) {
+        $(".menu a, .header__text").on("click", function (e) {
+            e.preventDefault();
+            const id = $(this).attr('href');
+            const top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    }
+
+    $(window).on('scroll resize', () => {
+        const screenWidth = $(window).width();
+        const diff = $(".about").offset().top - $(".header__top").offset().top;
+
+        $(".header__top").css('color', diff < 85 && screenWidth > 992 ? '#7792E0' : 'unset');
+
+        if (diff < 85) {
+            $(".menu .logo, .burger__button span, .burger__button::before, .burger__button::after")
+                .css('background-color', '#7792E0');
+        } else {
+            $(".menu .logo").css('background-color', 'unset');
+            $(".burger__button span, .burger__button::before, .burger__button::after")
+                .css('background-color', '#fff');
+        }
     });
 });
 
